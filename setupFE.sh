03642c31a4fb4a49d58f6a211b8dbddc610d3775
Thach Pham Van <92628305+kyojin2311@users.noreply.github.com>
2025-06-03 12:04:04 +0700

    fe

--- setupFE.sh
+++ setupFE.sh
@@ -22,7 +22,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 # echo "Configuring AWS CLI..."
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 # aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-# aws configure set default.region ap-southeast-1
+aws configure set default.region ap-southeast-1
 # aws configure set default.output json
 
 echo "[SETUP] Enabling and starting Docker..."
